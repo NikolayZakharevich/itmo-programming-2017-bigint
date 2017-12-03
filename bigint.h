96a80e533b8d4766b788065b652c1eaef2abce21
Nikolay <nikolayzakharevich@gmail.com>
2017-12-04 00:08:39 +0200

    Update bigint.h

--- bigint.h
+++ bigint.h
@@ -1,13 +1,16 @@
 #ifndef BIGINT_H
 #define BIGINT_H
 
+#include <stdbool.h>
+#include <ctype.h>
+#include <string.h>
+
 typedef struct bigInt {
     int amount;     //quantity of digits
     int digits[10000];
     _Bool isNegative;
     void (*print)(const struct bigInt*);
 } bigInt;
-
 
 bigInt BigInt (char *digitsString);  // constructor
 bool more (const bigInt *a, const bigInt *b);   // a > b
@@ -18,7 +21,5 @@ bigInt sum (const bigInt *a, const bigInt *b);  // a + b
 bigInt sub (const bigInt *a, const bigInt *b);  // a - b
 bigInt mul (const bigInt *a, const bigInt *b);  // a * b
 bigInt bdiv (const bigInt *a, const bigInt *b); // a / b'
-
-
 
 #endif // BIGINT_H
